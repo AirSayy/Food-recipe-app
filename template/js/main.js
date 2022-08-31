bbc37b19e858ac8b3040860b9c293110794afc6d
airsay <eruotorese@yahoo.com>
2022-08-31 15:28:43 +0100

    food recipe project day 3

--- template/js/main.js
+++ template/js/main.js
@@ -1,15 +1,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
+  
   const choice = document.querySelector('input').value
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+choice
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.drinks[0])
-        document.querySelector('#nameOfFood').innerHTML = data.drinks[0].strDrink
-        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        console.log(data.meals)
+        // document.querySelector('#nameOfFood').innerHTML = data.results[0].title
+        // document.querySelector('img').src = data.results[0].image
       })
       .catch(err => {
           console.log(`error ${err}`)
