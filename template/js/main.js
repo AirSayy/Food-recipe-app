5c7d6b18d379614fe63f15789879bacc5c81b97b
airsay <eruotorese@yahoo.com>
2022-08-31 16:38:08 +0100

    food recipe project 5

--- template/js/main.js
+++ template/js/main.js
@@ -11,7 +11,15 @@ function getFetch(){
         console.log(data.meals[0])
         document.querySelector('#nameOfFood').innerHTML = data.meals[0].strMeal
         document.querySelector('img').src = data.meals[0].strMealThumb
-        document.querySelector('iframe').src = data.meals[0].strYoutube
+        document.querySelector('#ing1').innerText = data.meals[0].strIngredient1
+        document.querySelector('#ing2').innerText = data.meals[0].strIngredient2
+        document.querySelector('#ing3').innerText = data.meals[0].strIngredient3
+        document.querySelector('#ing4').innerText = data.meals[0].strIngredient4
+        document.querySelector('#ing5').innerText = data.meals[0].strIngredient5
+        document.querySelector('#ing6').innerText = data.meals[0].strIngredient6
+        document.querySelector('#ing7').innerText = data.meals[0].strIngredient7
+        document.querySelector('#ing8').innerText = data.meals[0].strIngredient8
+
       })
       .catch(err => {
           console.log(`error ${err}`)
