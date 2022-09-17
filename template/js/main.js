70b4abf6e5d753998dc4c896aa23253855a628fa
airsay <eruotorese@yahoo.com>
2022-09-17 10:22:44 +0100

    update project

--- template/js/main.js
+++ template/js/main.js
@@ -8,18 +8,11 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.meals[0])
-        document.querySelector('#nameOfFood').innerHTML = data.meals[0].strMeal
-        document.querySelector('img').src = data.meals[0].strMealThumb
-        document.querySelector('#ing1').innerText = data.meals[0].strIngredient1
-        document.querySelector('#ing2').innerText = data.meals[0].strIngredient2
-        document.querySelector('#ing3').innerText = data.meals[0].strIngredient3
-        document.querySelector('#ing4').innerText = data.meals[0].strIngredient4
-        document.querySelector('#ing5').innerText = data.meals[0].strIngredient5
-        document.querySelector('#ing6').innerText = data.meals[0].strIngredient6
-        document.querySelector('#ing7').innerText = data.meals[0].strIngredient7
-        document.querySelector('#ing8').innerText = data.meals[0].strIngredient8
-
+        console.log(data.meals[0].strIngredient1)
+        console.log(data.meals[0].strIngredient2)
+        // document.querySelector('#nameOfFood').innerHTML = data.meals[0].strMeal
+        // document.querySelector('img').src = data.meals[0].strMealThumb
+        
       })
       .catch(err => {
           console.log(`error ${err}`)
