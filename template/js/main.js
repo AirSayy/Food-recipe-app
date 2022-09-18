d48ccdd62031c9c69bdb05b655d352455a0501d0
airsay <eruotorese@yahoo.com>
2022-09-18 21:18:01 +0100

    changed api link

--- template/js/main.js
+++ template/js/main.js
@@ -1,15 +1,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  
+  const apikey = '4ba5832eb2774d709a9694c5b6dfc569'
   const choice = document.querySelector('input').value
-  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${choice}`
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apikey}&addRecipeInformation=true&includeIngredients=true&query=${choice}&maxFat=25&number=2`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.meals[0].strIngredient1)
-        console.log(data.meals[0].strIngredient2)
+        console.log(data.results[0].image)
+        console.log(data.results[0].imageType)
+        
         // document.querySelector('#nameOfFood').innerHTML = data.meals[0].strMeal
         // document.querySelector('img').src = data.meals[0].strMealThumb
         
