74c9663e8fd2aaf35dba564b6382710a0deffc70
airsay <eruotorese@yahoo.com>
2022-10-19 11:53:45 +0100

    update

--- template/js/main.js
+++ template/js/main.js
@@ -10,12 +10,12 @@ function getFetch(){
 //  API URL 
   const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apikey}&addRecipeInformation=true&fillIngredients=true&query=${choice}`
 //  Fetches the Url request from the APi and responds as JSON
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data.results) // Returns Results of the data requested for
+fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+         console.log(data.results) // Returns Results of the data requested for
         
-        const title = data.results.map(el =>el.title) //Loops through the result and returns an array of the title name.
+         const title = data.results.map(el =>el.title) //Loops through the result and returns an array of the title name.
          console.log(title) // logs in the array in the console
          document.querySelector('#title').innerHTML = title[0] //Title of the meal choses shows in the dom
         
@@ -25,20 +25,20 @@ function getFetch(){
          document.querySelector('h4').innerHTML= data.results[0].summary // summary from the title of the recipe on the dom
        
 
-            let recipeInstructions = data.results[0].analyzedInstructions[0].steps.map(el => `<li>${el.step}</li>`).join('') // loops through the results, loops again through the analyzed instructions from the object and append it to the dom with the li's
-            document.querySelector('#recipe').innerHTML = recipeInstructions
+         let recipeInstructions = data.results[0].analyzedInstructions[0].steps.map(el => `<li>${el.step}</li>`).join('') // loops through the results, loops again through the analyzed instructions from the object and append it to the dom with the li's
+         document.querySelector('#recipe').innerHTML = recipeInstructions
            
-            const ingredients = data.results[0].extendedIngredients.map(el => `<li>${el.name}</li>`).join('') // loops through the results, loops again through the ingredients from the object and append it to the dom with the li's
-            document.querySelector('#ingredients').innerHTML =ingredients   
-            console.log(ingredients) 
+         const ingredients = data.results[0].extendedIngredients.map(el => `<li>${el.name}</li>`).join('') // loops through the results, loops again through the ingredients from the object and append it to the dom with the li's
+         document.querySelector('#ingredients').innerHTML =ingredients   
+         console.log(ingredients) 
             
-             const diet = data.results.map(el => `<li>${el.diets}</li>`) // loops through the diets from the object and append it to the dom with the li's
-             document.querySelector('#diet').innerHTML = diet[0]
+         const diet = data.results.map(el => `<li>${el.diets}</li>`) // loops through the diets from the object and append it to the dom with the li's
+         document.querySelector('#diet').innerHTML = diet[0]
 
 
         
-      })
-      .catch(err => {
-          console.log(`can't be found ${err}`)
-      });
+        })
+    .catch(err => {
+        console.log(`can't be found ${err}`)
+    });
 }
